0f74cc25e895aadaf7c91937188cdb7eb12ccd8a
Rubayed All Islam <rubayed@localhost.localdomain>
2024-01-09 16:52:06 +0600

    _

--- src/components/Accomplishments/Certifications/Certifications.jsx
+++ src/components/Accomplishments/Certifications/Certifications.jsx
@@ -14,30 +14,35 @@ function Certifications() {
       image: CertificateMachineLearning,
       issuedBy: 'Simplilearn Skillup',
       issuanceDate: 'January 9, 2024',
+      certificateLink: 'https://www.simplilearn.com/skillup-certificate-landing?token=eyJjb3Vyc2VfaWQiOiI2ODciLCJjZXJ0aWZpY2F0ZV91cmwiOiJodHRwczpcL1wvY2VydGlmaWNhdGVzLnNpbXBsaWNkbi5uZXRcL3NoYXJlXC90aHVtYl80NzcyNzA2XzE3MDQ3OTYxNTAucG5nIiwidXNlcm5hbWUiOiJSdWJheWVkIEFsbCBJc2xhbSJ9&utm_source=shared-certificate&utm_medium=lms&utm_campaign=shared-certificate-promotion&referrer=https%3A%2F%2Flms.simplilearn.com%2Fcourses%2F2789%2FMachine-Learning%2Fcertificate%2Fdownload-skillup&%24web_only=true&_branch_match_id=1273215162285138965&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXL87MLcjJ1EssKNDLyczL1k%2FVT872rcryMS5NcU8CAK4EJbclAAAA',
     },
     {
       title: 'Python',
       image: CertificatePython,
       issuedBy: 'Kaggle',
       issuanceDate: 'July 10, 2023',
+      certificateLink: 'https://www.kaggle.com/learn/certification/rubayedallislam/python',
     },
     {
       title: 'JavaScript Algorithms and Data Structures',
       image: CerticicateJavascript,
       issuedBy: 'FreeCodeCamp',
       issuanceDate: 'July 30, 2023',
+      certificateLink: 'https://www.freecodecamp.org/certification/RubayedMunna/javascript-algorithms-and-data-structures',
     },
     {
       title: 'Responsive Web Design',
       image: CertificateResponsiveWeb,
       issuedBy: 'FreeCodeCamp',
       issuanceDate: 'July 3, 2023',
+      certificateLink: 'https://www.freecodecamp.org/certification/RubayedMunna/responsive-web-design',
     },
     {
       title: 'Web Development Bootcamp',
       image: CertificateBongoDev,
       issuedBy: 'BongoDev',
       issuanceDate: 'January 3, 2024',
+      certificateLink: 'https://bongodev.com/',
     },
     
     {
@@ -45,14 +50,16 @@ function Certifications() {
       image: CertificateICPCPreli2022,
       issuedBy: 'International Collegiate Programming Contest (ICPC)',
       issuanceDate: 'Septermber 3, 2022',
+      certificateLink: 'https://icpc.global/',
     },
     {
       title: 'ICPC Dhaka Regional Preliminary Contest 2021',
       image: CertificateICPCPreli2021,
       issuedBy: 'International Collegiate Programming Contest (ICPC)',
       issuanceDate: 'April 7, 2021',
+      certificateLink: 'https://icpc.global/',
     },
-    //hello
+   
     // Add more certifications as needed
   ]);
 
@@ -65,7 +72,8 @@ function Certifications() {
             <div className="bg-gray-0 flex items-center justify-center ">
               <div className="max-w-full bg-white p-8 rounded-lg shadow-lg border-r-2 border-t-8 border-t-blue-700">
                 {/* Certification Title */}
-                <h2 className="text-2xl font-bold mb-4">{certification.title}</h2>
+                <a href={certification.certificateLink} target='_blank'><h2 className="text-2xl font-bold mb-4">{certification.title}</h2></a>
+                
                 {/* Certification Image */}
                 <div className="my-4 w-full">
                   <img src={certification.image} alt="Certification Image" className="w-full h-full object-cover" />
