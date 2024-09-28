e0cbf6ea230ff05f8c6f3ef2fa3ce24d83d7989e
Rubayed All Islam <rubayed@localhost.localdomain>
2024-09-28 12:49:59 +0600

    -

--- src/components/ExtraCurricular/Extracurricular.jsx
+++ src/components/ExtraCurricular/Extracurricular.jsx
@@ -89,20 +89,20 @@ function Extracurricular() {
                                     {/* Event Date */}
                                     <div className="flex items-center mt-2 text-gray-600">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mr-2">
-                                            <path fillRule="evenodd" d="M6 2.25a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 .75.75v.75h.75A2.25 2.25 0 0 1 21 5.25v15A2.25 2.25 0 0 1 18.75 22.5H5.25A2.25 2.25 0 0 1 3 20.25v-15A2.25 2.25 0 0 1 5.25 3h.75v-.75ZM5.25 4.5A.75.75 0 0 0 4.5 5.25v15c0 .414.336.75.75.75h13.5a.75.75 0 0 0 .75-.75v-15a.75.75 0 0 0-.75-.75H17.5v.75a.75.75 0 0 1-1.5 0V3H8v.75a.75.75 0 0 1-1.5 0V3H5.25Zm1.5 4.5a.75.75 0 0 1 1.5 0v8.25a.75.75 0 0 1-1.5 0V9Zm9.75 0a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Zm-6.75 0a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Z" clipRule="evenodd" />
+                                            <path fillRule="evenodd" d="M6 2.25a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 .75.75v.75h.75A2.25 2.25 0 0 1 21 5.25v13.5A2.25 2.25 0 0 1 18.75 21H5.25A2.25 2.25 0 0 1 3 18.75V5.25A2.25 2.25 0 0 1 5.25 3h.75v-.75Zm1.5 1.5v.75h10.5v-.75H7.5Z" clipRule="evenodd" />
                                         </svg>
                                         <span>{competition.eventDate}</span>
                                     </div>
 
                                     {/* Status */}
                                     <div className="flex items-center mt-2">
-                                        <span className="bg-gray-200 text-gray-700 font-semibold py-1 px-2 rounded-lg mr-4">{competition.status}</span>
+                                        <span className="bg-green-500 text-gray-100 font-semibold py-1 px-2 rounded-lg mr-4">{competition.status}</span>
                                     </div>
 
                                     {/* Organizer */}
                                     <div className="flex items-center mt-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mr-2 text-gray-600">
-                                            <path d="M21.721 12.752a9.711 9.711 0 0 0-.945-5.003 12.754 12.754 0 0 1-4.339 2.708 18.991 18.991 0 0 1-.214 4.772 17.165 17.165 0 0 0 5.498-2.477ZM14.634 15.55a17.324 17.324 0 0 0 .332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 0 0 .332 4.647 17.385 17.385 0 0 0 5.268 0ZM9.772 17.119a18.963 18.963 0 0 0 4.456 0A17.182 17.182 0 0 1 12 21.724a17.18 17.18 0 0 1-2.228-4.605ZM7.777 15.23a18.87 18.87 0 0 1-.214-4.774 12.753 12.753 0 0 1-4.34-2.708 9.711 9.711 0 0 0-.944 5.004 17.165 17.165 0 0 0 5.498 2.477ZM21.356 14.752a9.765 9.765 0 0 1-7.478 6.817 18.64 18.64 0 0 0 1.988-4.718 18.627 18.627 0 0 0 5.49-2.098ZM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 0 0 1.988 4.718 9.765 9.765 0 0 1-7.478-6.816ZM13.878 2.43a9.755 9.755 0 0 1 6.116 3.986 11.267 11.267 0 0 1-3.746 2.504 18.63 18.63 0 0 0-2.37-6.49ZM12 2.276a17.152 17.152 0 0 1 2.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0 1 12 2.276ZM10.122 2.43a18.629 18.629 0 0 0-2.37 6.49 11.266 11.266 0 0 1-3.746-2.504 9.754 9.754 0 0 1 6.116-3.985Z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
                                         </svg>
                                         {/* <span>{competition.organizer}</span> */}
                                         <span><a href={competition.organizerWebsite} className="text-blue-600 ml-2 hover:underline" target='_blank' rel="noopener noreferrer">{competition.organizer}</a></span>
