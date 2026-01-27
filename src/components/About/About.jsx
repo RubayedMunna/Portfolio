5501fdd8bef55cfccff0ba22111cac3972c5f863
RubayedMunna <jucse29.370@gmail.com>
2026-01-27 23:14:02 +0600

    about updted

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ function About() {
       id: 1,
       title: 'Introduction',
       content:
-        'I am Rubayed All Islam, a Lecturer in Computer Science and Engineering at Daffodil International University, currently pursuing my M.Sc. from Jahangirnagar University. I’m passionate about teaching, research, and lifelong learning.',
+        'I am Rubayed All Islam, a Lecturer in Computer Science and Engineering at Southeast University, currently pursuing my M.Sc. from Jahangirnagar University. I’m passionate about teaching, research, and lifelong learning.',
       icon: '👋',
     },
     {
